31a5e4880ce8d8eac062fdc649fdc9b870549ea8
sansiddhjain <sansiddh@gmail.com>
2019-01-29 11:21:13 +0530

    Adding spatial pages

--- mtp_website/create-geojson.py
+++ mtp_website/create-geojson.py
@@ -3,16 +3,20 @@ import geopandas as gpd
 import os 
 import numpy as np 
 
-df_data = pd.read_csv("data/asset-ownership.csv")
+df_shapefile = gpd.read_file("GeoJSON_Files/Shape Files/2011/2011_Dist.shp")
 df_shapefile = gpd.read_file("GeoJSON_Files/backup.geojson")
-# df_shapefile = df_shapefile[df_shapefile["censuscode"] != 0]
 
+# Create GeoJSON files for the 2011 Census asset ownership labels!!
+
+df_data = pd.read_csv("data/asset-ownership.csv")
 del df_data["Name"]
 df_data.columns = ["censuscode", "asset"]
 
 df_shapefile = df_shapefile.merge(df_data, on="censuscode")
 
 print(df_shapefile)
+
+# Create GeoJSON files for the 2011 labels, 2001 labels, and the change labels!!
 
 df_data = pd.read_csv("data/2001-change.csv")
 df_data.columns = [x.lower() for x in df_data.columns]
@@ -28,3 +32,20 @@ df_shapefile = df_shapefile.ix[:, cols]
 print(df_shapefile)
 
 df_shapefile.to_file(driver='GeoJSON', filename='GeoJSON_Files/main.geojson')
+
+# Create GeoJSON files for the spatial labels!!
+
+df_data = pd.read_csv("data/clustering-labels.csv")
+df_data.columns = [x.lower() for x in df_data.columns]
+del df_data["unnamed: 0"]
+df_data.columns = ["censuscode" if x == "district" else x for x in df_data.columns]
+df_data.columns = [x+"_spatial" if x != "censuscode" else x for x in df_data.columns]
+df_shapefile = df_shapefile.merge(df_data, on="censuscode", how="outer")
+
+cols = list(df_shapefile)
+cols.insert(len(cols)-1, cols.pop(cols.index('geometry')))
+df_shapefile = df_shapefile.ix[:, cols]
+
+print(df_shapefile)
+
+df_shapefile.to_file(driver='GeoJSON', filename='GeoJSON_Files/main-2.geojson')
--- mtp_website/create-webpages.py
+++ mtp_website/create-webpages.py
@@ -11,7 +11,7 @@ full_form_dict = {
 
 for type1 in ['2001', '2011', 'change']:
 	for type2 in ['bf', 'chh', 'fc', 'emp', 'f_emp', 'msl', 'msw']:
-		main_file = open('bf_adv.html', 'r')
+		main_file = open('trilabel-broadcasting.html', 'r')
 		lines = main_file.readlines()
 		
 		for i in range(119, len(lines)):
@@ -20,11 +20,33 @@ for type1 in ['2001', '2011', 'change']:
 			line = line.replace('Asset Ownership', full_form_dict[type2]+' '+type1.title())
 			if type1 == 'change':
 				line = line.replace('rudimentary', 'decline')
+				line = line.replace('Rudimentary', 'Decline')
 				line = line.replace('intermediate', 'no change')
+				line = line.replace('Intermediate', 'No Change')
 				line = line.replace('advanced', 'increase')
+				line = line.replace('Advanced', 'Increase')
 			lines[i] = line
 
 		with open(type1+'/'+type2+'.html', 'w') as f:
 			f.writelines(lines)
 		print(type1+'/'+type2+'.html'+' done.')
 	
+for type1 in ['spatial']:
+	for type2 in ['bf', 'chh', 'fc', 'emp', 'msl', 'msw']:
+		if type2 != 'emp':
+			type3_list = ['adv', 'rud', 'int']
+		else:
+			type3_list = ['un', 'al', 'nal']
+		for type3 in type3_list:
+			main_file = open('spatial-broadcasting.html', 'r')
+			lines = main_file.readlines()
+			
+			for i in range(169, len(lines)):
+				line = lines[i]
+				line = line.replace('emp_al_spatial', type2+'_'+type3+'_'+type1)
+				line = line.replace('EMP_AL', type2.upper()+'_'+type3.upper())
+				lines[i] = line
+			with open(type1+'/'+type2+'_'+type3+'.html', 'w') as f:
+				f.writelines(lines)
+			print(type1+'/'+type2+'_'+type3+'.html'+' done.')
+		
